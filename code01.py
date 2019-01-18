71dd35ff4cbf2e9389dd539bc2e9a5b8de3f6ce8
Diadems <accost.abhishek@gmail.com>
2019-01-18 11:29:59 -0500

    sample code to test jenks

---
+++ code01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#single line execution separated by semi-colon
+import sys; x="foobar"; sys.stdout.write(x+ '\n')
+
+#printing to standard out
+greet="This is initial greetings to start with"
+print ("\nhello world!\n")
+print (greet)
+
+#accepting user imput
+name=input ("\nWhats your name? ")
+print ("Have fun "+name)
+
+# Declaring a multiline paragraph in a variable below
+paragraf="""Writing this down as this is
+supposed to go to this next line as well"""
+
+#priting a multiline sentence
+print (paragraf)
+input("\n\nPress enter to exit this code\n")
+print ("\nexiting...")
+
